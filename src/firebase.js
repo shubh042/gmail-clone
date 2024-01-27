3089f12b5b6b143b9b7ba9e79237e6546b6b2ebe
shubham dhumal <94701914+sd025@users.noreply.github.com>
2024-01-27 21:26:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA6_q-k8EOD9Sc3Pj7JCEOvl3ht1rnsCm4",
-    authDomain: "clone-685b5.firebaseapp.com",
-    projectId: "clone-685b5",
-    storageBucket: "clone-685b5.appspot.com",
-    messagingSenderId: "177690054609",
-    appId: "1:177690054609:web:0818f130f48912bd2f5af0"
+      apiKey: "AIzaSyBLTCmREH1721yObudoBNP6dYcZ1aiVtpc",
+      authDomain: "chat-pdf-7e36a.firebaseapp.com",
+      projectId: "chat-pdf-7e36a",
+      storageBucket: "chat-pdf-7e36a.appspot.com",
+      messagingSenderId: "287055962278",
+      appId: "1:287055962278:web:36c0d66cb32d81aa4bd467"
   };
   
   const firebaseApp = firebase.initializeApp(firebaseConfig);
